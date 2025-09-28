95ad2bfe1cfc4b345b9e791584de042321c43a7b
Kei Watanabe <taiyaky@me.com>
2025-09-28 22:48:59 +0900

    Modify generator spec

--- spec/generator_spec.rb
+++ spec/generator_spec.rb
@@ -51,13 +51,77 @@ RSpec.describe 'Verikloak::Audience::Generators::InstallGenerator' do
     load File.expand_path('../lib/generators/verikloak/audience/install/install_generator.rb', __dir__)
   end
 
-  it "creates initializer" do
+  it "creates initializer with expected content at the default location" do
+    Dir.mktmpdir do |dir|
+      Dir.chdir(dir) do
+        generator = Verikloak::Audience::Generators::InstallGenerator.new
+        generator.create_initializer
+
+        path = 'config/initializers/verikloak_audience.rb'
+        expect(File).to exist(path)
+        
+        initializer_content = File.read(path)
+        expect(initializer_content).to include('Verikloak::Audience::Railtie')
+        expect(initializer_content).to include('Verikloak::Audience::Railtie.insert_middleware')
+        expect(initializer_content).to include('Rails.application')
+        expect(initializer_content).to include('frozen_string_literal: true')
+      end
+    end
+  end
+
+  it "creates the initializer with all expected content sections" do
     Dir.mktmpdir do |dir|
       Dir.chdir(dir) do
         gen = Verikloak::Audience::Generators::InstallGenerator.new
         gen.create_initializer
 
-        expect(File).to exist('config/initializers/verikloak_audience.rb')
+        initializer_content = File.read('config/initializers/verikloak_audience.rb')
+
+        expect(initializer_content).to include('defined?(Rails)')
+        expect(initializer_content).to include('Rails.respond_to?(:application)')
+        expect(initializer_content).to include('frozen_string_literal: true')
+      end
+    end
+  end
+
+  it "creates config directory structure when it doesn't exist" do
+    Dir.mktmpdir do |dir|
+      Dir.chdir(dir) do
+        gen = Verikloak::Audience::Generators::InstallGenerator.new
+        gen.create_initializer
+
+        expect(Dir).to exist('config/initializers')
+      end
+    end
+  end
+
+  context "error handling" do
+    it "handles missing template file gracefully" do
+      Dir.mktmpdir do |dir|
+        Dir.chdir(dir) do
+          # Temporarily change source_root to simulate missing template
+          fake_source_root = File.join(dir, 'fake_templates')
+          
+          # Override the source_root class method
+          allow(Verikloak::Audience::Generators::InstallGenerator).to receive(:source_root).and_return(fake_source_root)
+          
+          generator = Verikloak::Audience::Generators::InstallGenerator.new
+          
+          expect { generator.create_initializer }.to raise_error(Errno::ENOENT)
+        end
+      end
+    end
+
+    it "raises an error when destination directory cannot be created due to file conflict" do
+      Dir.mktmpdir do |dir|
+        Dir.chdir(dir) do
+          # Create a file with the same name as the directory we want to create
+          FileUtils.touch('config')
+          
+          generator = Verikloak::Audience::Generators::InstallGenerator.new
+          
+          expect { generator.create_initializer }.to raise_error(Errno::EEXIST)
+        end
       end
     end
   end
